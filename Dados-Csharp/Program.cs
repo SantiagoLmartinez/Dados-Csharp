feae2ef244ff1a401d30bde6dc1d2469c2da67ff
slm17 <slm17slm17@gmail.com>
2022-07-26 12:57:23 -0300

    Agregar archivos de proyecto.

---
+++ Dados-Csharp/Program.cs
@@ -0,0 +1,5 @@
+﻿//Hacer un programa que imprima los numeros de un dado por consola (1 al 6) de manera aleatorea
+
+Random dado = new Random();
+int tirarDado = dado.Next(1, 7);
+Console.WriteLine(tirarDado);
